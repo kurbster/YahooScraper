b2cf92c240fcd251805cdeb36b42783fd4faea29
kurbster <kkuznia28@gmail.com>
2020-08-05 17:18:04 -0700

    Added in pandas support
    I now have my data in a pandas DataFrame. Now I am working on saving the data into a csv so I don't have to read data from the web I already have.

--- WebScraper.py
+++ WebScraper.py
@@ -18,14 +18,13 @@ def get_fin_data(url, attrs, tickers):
     web_attrs = attrs
     stocks = tickers
     for stock in stocks:
-        income_statement = parse_page('financials', stock)
+        #income_statement = parse_page('financials', stock)
         #balance_sheet = parse_page('balance-sheet', stock)
         #cash_flow = parse_page('cash-flow',         stock)
         price_page = parse_page(None,               stock)
-        print(income_statement)
         
         
-def parse_page(key='financials', stock='AAPL'):
+def parse_page(key, stock):
     # If the key is None then I want to get the front page of the app
     # Which contains the current price of the stock
     if key is None:
@@ -35,10 +34,9 @@ def parse_page(key='financials', stock='AAPL'):
         soup = BeautifulSoup(page.content, 'html.parser')
         regex = re.compile('W(100%)*')
         table = soup.find_all('table', class_=regex)
-    
+        for t in table:
+            print(t.text)
     else:
-        key = 'financials'
-        stock ='AAPL'
         # I get the url associated with what ever is passed in
         url = base_url + stock + '/' + key + web_attrs['stock_key'] + stock
         # Then I get the driver object and the data table
@@ -51,24 +49,42 @@ def parse_page(key='financials', stock='AAPL'):
         
         # This gives me the top of the table so I can find the column names
         tabl_info = soup.find('div', class_='D(tbr)')
-        cols = tabl_info.find_all('span')
+        columns = tabl_info.find_all('span')
         
         # This gives me the data table
         tabl = soup.find('div', class_='D(tbrg)')
         data = tabl.find_all('div', {'data-test' : 'fin-col'})
         names = tabl.find_all('span', class_='Va(m)')
         
+        # These are the lists and dictionary I will use to create my pd DataFrame
         temp = {}
+        rows = []
+        cols = []
+        # Columns represents the dates we are taking
+        for c in columns:
+            cols.append(c.text)
+        # I pop the first value from this list because it is just a placeholder and I dont need it
+        cols.pop(0)
+        # Names represents the type of data we are taking
+        for n in names:        
+            rows.append(n.text)
+        
+        # Data represents that numbers we are taking in. I am using enumerate
+        # Here so I can turn the data into a dictionary 
         for i,d in enumerate(data):
-            row_num = i // (len(cols) - 1)
-            if i % (len(cols) - 1) == 0:
-                temp[row_num] = [d.text]
+            row_num = i // len(cols)
+            # If this is the first entry in a row we have to make the list
+            if i % len(cols) == 0:
+                temp[rows[row_num]] = [d.text]
+            # Otherwise I append the value to the list
             else:
-                temp[row_num].append(d.text)
+                temp[rows[row_num]].append(d.text)
         
-        
+        # The orient (or rows) of my data is stored in the key (or index)
+        # Of my dictionary, therefore I just used those values
+        df = pd.DataFrame.from_dict(temp, orient='index', columns=cols)
         driver.close()
-        
+        return df
     
 
 def click_buttons(element):
