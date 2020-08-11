b46767044cbdbcb3118e9495ed1a967d05e77144
kurbster <kkuznia28@gmail.com>
2020-08-10 17:15:47 -0700

    Update parse_page
    Now the parse_page function takes in any amount of arguments then based on how many are passed in, it will return the corresponding DataFrame

--- WebScraper.py
+++ WebScraper.py
@@ -5,50 +5,49 @@ Created on Tue Aug  4 13:37:46 2020
 @author: karby
 """
 
-from selenium import webdriver
-from bs4 import BeautifulSoup
 import pandas as pd
 import os
 import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+#from pandas_datareader import data as wb
 
-def get_fin_data(url, attrs, tickers):
-    global stocks, base_url, web_attrs
-    base_url = url
-    web_attrs = attrs
-    stocks = tickers
+'''
+    get_fin_data will get the financial data for the specific stock passed in
+    First it will get all of the historic financial data right now that means 
+    Income statement, balance sheet, and cash flow statement.
+        
+        To save time everytime I read a page from yahoo finance I save
+        The resulting DataFrame to a csv. Then if the csv exists I just read
+        That instead of creating a new selenium driver object.
+'''
+
+def get_fin_data(url, attrs, stock, **kw):
     fin_data = {}
     # TODO maybe take in a list of pages you want to get
     # i.e data = [financials, balance-sheet, cash-flow] then iterate through that
-    for stock in stocks:
-        # If we did not create a new folder then read the data
-        # Both of these methods return a pd DataFrame
-        path = create_folder(stock)
-        if not path:
-            income_statement = read_csv(stock, 'income.csv')
-            balance_sheet    = read_csv(stock, 'balance.csv')
-            cash_flow        = read_csv(stock, 'cash.csv')
-        else:
-            income_statement = parse_page('financials',    stock)
-            balance_sheet    = parse_page('balance-sheet', stock)
-            cash_flow        = parse_page('cash-flow',     stock)
-            write_csv(path, 'income.csv',  income_statement)
-            write_csv(path, 'balance.csv', balance_sheet)
-            write_csv(path, 'cash.csv',    cash_flow)
-        # fin_data is a dict that maps the stock name to another dict 
-        # Which maps a string to the DataFrame corresponding to that string
-        fin_data[stock] = {'income' : income_statement, 'balance' : balance_sheet
-                           , 'cash' : cash_flow}
-    return fin_data
-
-def get_stock_data(url, attrs, tickers):
-    global stocks, base_url, web_attrs
-    base_url = url
-    web_attrs = attrs
-    stocks = tickers
-    fin_data = {}
-    for stock in stocks:
-        stock_info = parse_page(None, stock)
-        fin_data[stock] = stock_info
+    
+    # If the path wasn't created then just read the csv
+    path = create_folder(stock)
+    if not path:
+        income_statement = read_csv(stock, 'income.csv')
+        balance_sheet    = read_csv(stock, 'balance.csv')
+        cash_flow        = read_csv(stock, 'cash.csv')
+    else:
+        #Here I have to pass in the specific page I want to parse
+        income_statement = parse_page(stock, url, attrs, 'financials')
+        balance_sheet    = parse_page(stock, url, attrs, 'balance-sheet')
+        cash_flow        = parse_page(stock, url, attrs, 'cash-flow')
+        write_csv(path, 'income.csv',  income_statement)
+        write_csv(path, 'balance.csv', balance_sheet)
+        write_csv(path, 'cash.csv',    cash_flow)
+    
+    
+    fin_data[stock] = {'income'   : income_statement, 
+                       'balance'  : balance_sheet,
+                       'cash'     : cash_flow, 
+                       'CurrentP' : parse_page(stock, url, attrs)
+                       }
     return fin_data
 
 # Making the parent directory point to our financial data folder
@@ -84,28 +83,44 @@ def read_csv(stock, file_name):
 def write_csv(path, file_name, data):
     path = os.path.join(path, file_name)
     data.to_csv(path)
+    
+'''
+    parse_page will always take 1 argument and that is the stock name
+    Then it will either take 2 extra arguments (url, attrs)
+                         OR  3 extra arguments (url, attrs, key)
+                         OR  only keyword arguments
+    3 arguments means that I am taking the summary data
+    4 arguments means that I am taking specific financial data
+    
+    Only keword arguments that can tells the method that we want to find
+    The historical price data for a stock. Therefore I can us pandas_datareader
+    And there is no need to pass in a url or any web attributes
+    
+            The specific keywords that can be passed in are:
+                start=''
+                end=''
+                data_source='yahoo'
+            These are the kwargs passed into wb.DataReader
+'''
 
-def parse_page(key, stock):
-    # If the key is None then I want to get the front page of the app
-    # Which contains the current price of the stock
-    if key is None:
-        url = base_url + stock + web_attrs['stock_key'] + stock
+def parse_page(stock, *args):
+    if len(args) == 2:
+        url, attrs = args
         # There are no buttons I need to press on this page so no need for selenium
-        page = requests.get(url)
+        page = requests.get(url + stock + attrs['stock_key'] + stock)
         soup = BeautifulSoup(page.content, 'html.parser')
-        table = soup.find_all('table')[0]
+        table = soup.find_all('table')
         df = pd.read_html(str(table))
-    else:
-        # I get the url associated with what ever is passed in
-        url = base_url + stock + '/' + key + web_attrs['stock_key'] + stock
+    elif len(args) == 3:
+        url, attrs, key = args
         # Then I get the driver object and the data table
         driver = webdriver.Chrome('C:\\Users\\karby\\Desktop\\Python Files\\ChromeDriver\\chromedriver')
-        driver.get(url)
+        driver.get(url + stock + '/' + key + attrs['stock_key'] + stock)
         table = driver.find_element_by_xpath('//*[@id="Col1-1-Financials-Proxy"]/section/div[4]/div[1]/div[1]/div[2]')
         click_buttons(table)
         # Once all of the buttons are pushed I can parse the page
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        
+    
         # This gives me the top of the table so I can find the column names
         tabl_info = soup.find('div', class_='D(tbr)')
         columns = tabl_info.find_all('span')
@@ -130,7 +145,7 @@ def parse_page(key, stock):
             # If this is the first entry in a row we have to make the list
             if i % len(cols) == 0:
                 temp[rows[row_num]] = [d.text]
-            # Otherwise I append the value to the list
+                # Otherwise I append the value to the list
             else:
                 temp[rows[row_num]].append(d.text)
         
@@ -138,8 +153,8 @@ def parse_page(key, stock):
         # Of my dictionary, therefore I just used those values
         df = pd.DataFrame.from_dict(temp, orient='index', columns=cols)
         driver.close()
-    return df
-    
+    return df     
+
 
 def click_buttons(element):
     # First I get all the children of the web element being passed in
--- main.py
+++ main.py
@@ -6,20 +6,39 @@ Created on Tue Aug  4 13:18:41 2020
 """
 
 import WebScraper as scraper
-from Watcher import Watcher
+
+'''
+    base_url doesn't change between calls nor the different attributes
+    To help you navigate the website the only one needed is '?p='
+    Therefore each page only differs in the stock name
+    e.x
+    This will get the stock chart for AAPL
+        https://finance.yahoo.com/quote/AAPL?p=AAPL
+    
+    This will get the balance sheet for AAPL
+        https://finance.yahoo.com/quote/AAPL/balance-sheet?p=AAPL
+    
+'''
 
 base_url = 'https://finance.yahoo.com/quote/'
-# These are the generic attrbutes for calling the proper html
 web_attrs = {'stock_key' : '?p='}
 
 # This is the different type of data we want to get
 # And below are the stocks we want to get
-tickers = ['AAPL', 'GOOG', 'FB', 'WMT', 'TSLA', 'MRNA']
+tickers = ['AAPL', 'GOOG', 'FB', 'WMT', 'TSLA', 'MRNA', 'F', 'BRK-A']
 
-# This is a dictionary mapping the ticker to a dictionary
-# Containing the financial data mapped to its name
-financial_data = scraper.get_fin_data(base_url, web_attrs, tickers)
-#stock_data     = scraper.get_stock_data(base_url, web_attrs, tickers)
+'''
+    This dictionary will map a stocks name to another dictionary
+    That dictionary maps the name of a dataframe then that DF
+    e.x
+        financial_data = {'AAPL' : {'income'   : pd.DataFrame,
+                                    'balance'  : pd.DataFrame,
+                                    'cash'     : pd.DataFrame,
+                                    'historic' : pd.DataFrame,
+                                    'current'  : pd.DataFrame}}
+'''
+financial_data = {}
 
-watcher = Watcher(tickers)
-watcher.watch(0, 0, 5)
+for t in tickers:
+    financial_data[t] = scraper.get_fin_data(base_url, web_attrs, t)
+    
